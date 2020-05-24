1692ca87456a79850af019016060d9d717e1faeb
MargaritaHazana <margaritahazana@gmail.com>
2020-05-23 21:05:35 -0300

    Views del home, detail, cart y forms

--- site/src/routes/index.js
+++ site/src/routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { view: 'index' });
+});
+
+router.get('/productDetail', function(req, res) {
+  res.render('productDetail', { view: 'detail' });
+});
+
+router.get('/productCart', function(req, res) {
+  res.render('productCart', { view: 'carrito' });
+});
+
+router.get('/productAdd', function(req, res) {
+  res.render('productAdd', { view: 'forms' });
+});
+
+router.get('/register', function(req, res) {
+  res.render('register', { view: 'forms' });
 });
 
 module.exports = router;

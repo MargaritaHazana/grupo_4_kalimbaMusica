ce98733b77eb38c58f75636f9cdc5a8155169b58
JoacoGirod <joaquingirod@gmail.com>
2020-07-13 21:44:28 -0300

    minor fix en el edit de imagenes

--- site/src/controllers/productController.js
+++ site/src/controllers/productController.js
@@ -151,8 +151,13 @@ productController = {
             let producto = await DB.Product.findByPk ( idProducto,{ include: [ 'colors','brands','subcategories','categories','types','images' ] } )
             let imagen = await DB.Image.findByPk (producto.images[0].id)
             // update de caracteriticas del producto y tablas relacionadas 1:n
-            await producto.update(req.body)  
-            await imagen.update({name : req.files[0].filename})       
+            
+            await producto.update(req.body)   
+            if(req.files[0] != undefined){
+                await imagen.update({name : req.files[0].filename})
+            }
+             
+                
              // update de relaciones n:m
             await producto.removeColor(producto.colors)
             await producto.addColor(req.body.colors)

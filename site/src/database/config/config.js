ac7cd031a2f60ca82c9cb7dc7a91257f1c2afc61
ana <anagomezcrov33@gmail.com>
2020-07-16 23:16:38 -0300

    arreglos js-front

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "kalimba_musica",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": 3306,
+    "port": 8889,
     "define": {
       "paranoid":true,
     }
